2a3de176a7f8d66d45082ec0fee076563cc5f85a
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-09 23:21:55 +0000

    fixed whitespaceses

--- main.cs
+++ main.cs
@@ -20,35 +20,33 @@ class Program
         integer2 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
 
-
-        if (integer1 > 0 && integer2 > 0) 
+        if (integer1 > 0 && integer2 > 0)
         {
-            while (counter < integer2) 
+            while (counter < integer2)
             {
             answer = answer + integer1;
             counter = counter + 1;
             }
-        } 
-        else if (integer1 > 0 && integer2 < 0) 
+        }
+        else if (integer1 > 0 && integer2 < 0)
         {
-            while (counter > integer2) 
+            while (counter > integer2)
             {
             answer = answer - integer1;
             counter = counter - 1;
-            } 
-        } 
-        else if (integer1 < 0 && integer2 > 0) 
+            }
+        }
+        else if (integer1 < 0 && integer2 > 0)
         {
-            while (counter > integer1) 
+            while (counter > integer1)
             {
                 answer = answer - integer2;
                 counter = counter - 1;
             }
-
-        } 
-        else if (integer2 < 0 && integer1 < 0) 
+        }
+        else if (integer2 < 0 && integer1 < 0)
         {
-            while (counter > integer2) 
+            while (counter > integer2)
             {
                 answer = answer - integer1;
                 counter = counter - 1;
