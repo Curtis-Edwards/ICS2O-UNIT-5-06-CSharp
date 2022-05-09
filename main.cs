c70c9f5dd70b91c4e9466b1f9ae069ae427a824e
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-09 23:16:56 +0000

    fixed formating

--- main.cs
+++ main.cs
@@ -16,7 +16,7 @@ class Program
         Console.Write("Integer 1: ");
         integer1 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
-        Console.Write("Integer 2:");
+        Console.Write("Integer 2: ");
         integer2 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
 
@@ -25,24 +25,24 @@ class Program
         {
             while (counter < integer2) 
             {
-            answer = answer + integer1
-            counter = counter + 1
+            answer = answer + integer1;
+            counter = counter + 1;
             }
         } 
         else if (integer1 > 0 && integer2 < 0) 
         {
             while (counter > integer2) 
             {
-            answer = answer - integer1
-            counter = counter - 1
+            answer = answer - integer1;
+            counter = counter - 1;
             } 
         } 
         else if (integer1 < 0 && integer2 > 0) 
         {
             while (counter > integer1) 
             {
-                answer = answer - integer2
-                counter = counter - 1
+                answer = answer - integer2;
+                counter = counter - 1;
             }
 
         } 
@@ -50,8 +50,8 @@ class Program
         {
             while (counter > integer2) 
             {
-                answer = answer - integer1
-                counter = counter - 1
+                answer = answer - integer1;
+                counter = counter - 1;
             }
         }
         Console.WriteLine(answer);
