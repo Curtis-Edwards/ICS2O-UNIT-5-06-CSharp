e7bc4f7c2b23e108645f8ea5cfb7a8716a41a08a
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-09 19:09:15 -0400

    updated README

--- main.cs
+++ main.cs
@@ -5,19 +5,56 @@ class Program
     public static void Main(string[] args)
     {
         // This function accepts user input
-        int radius;
-        double pi = Math.PI;
-        double area;
+        int integer1;
+        int integer2;
+        var answer = 0;
+        var counter = 0;
 
-        Console.WriteLine("Please enter the radius of the circle");
+        Console.WriteLine("Please enter your integers");
         Console.WriteLine("");
 
-        Console.Write("radius: ");
-        radius = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Integer 1: ");
+        integer1 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
-        area = (pi * radius) * radius;
+        Console.Write("Integer 2:");
+        integer2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("");
 
-        Console.WriteLine("Area: " + area.ToString("0.00") + "cm²");
+
+        if (integer1 > 0 && integer2 > 0) 
+        {
+            while (counter < integer2) 
+            {
+            answer = answer + integer1
+            counter = counter + 1
+            }
+        } 
+        else if (integer1 > 0 && integer2 < 0) 
+        {
+            while (counter > integer2) 
+            {
+            answer = answer - integer1
+            counter = counter - 1
+            } 
+        } 
+        else if (integer1 < 0 && integer2 > 0) 
+        {
+            while (counter > integer1) 
+            {
+                answer = answer - integer2
+                counter = counter - 1
+            }
+
+        } 
+        else if (integer2 < 0 && integer1 < 0) 
+        {
+            while (counter > integer2) 
+            {
+                answer = answer - integer1
+                counter = counter - 1
+            }
+        }
+        Console.WriteLine(answer);
 
         Console.WriteLine("\nDone.");
     }
